7b90a1e8f15b6e24e8b505a90c006ea67b8124ca
sunbinqi <sunbinqi@wdtrip.com>
2023-04-20 14:04:43 +0800

    接入koa

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
-const Koa = require('koa');
-const app = new Koa();
+import cors from '@koa/cors';
+import bodyParser from 'koa-bodyparser';
+import Koa from 'koa';
 
+const app = new Koa();
+//跨域访问限制
+app.use(
+	cors({
+		origin: '*',
+		maxAge: 3600,
+		credentials: true,
+		methods: 'POST,OPTIONS,PUT,HEAD',
+		headers: ' Authentication,Origin, X-Requested-With, Content-Type, Accept,token'
+	})
+);
+app.use(bodyParser());
 // response
 app.use(ctx => {
-  ctx.body = 'Hello Koa';
+  ctx.body = '草';
 });
+
 
 app.listen(3000);
