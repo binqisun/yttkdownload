8ec200ec759cfabb0068d4b651953061d259fd78
sunbinqi <sunbinqi@wdtrip.com>
2023-04-20 11:47:20 +0800

    set for Koa

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-var http = require('http');
+const Koa = require('koa');
+const app = new Koa();
 
-http.createServer(function (request, response) {
+// response
+app.use(ctx => {
+  ctx.body = 'Hello Koa';
+});
 
-    // 发送 HTTP 头部
-    // HTTP 状态值: 200 : OK
-    // 内容类型: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-
-    // 发送响应数据 "Hello World"
-    response.end('Hi Hello');
-}).listen(8888);
-
-// 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+app.listen(3000);
